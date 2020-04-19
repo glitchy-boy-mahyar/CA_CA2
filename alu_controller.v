1df96483af3c7fa62f6d345b2cc9984fcc3abb04
Alireza Salamat <52575958+alirezasalamat@users.noreply.github.com>
2020-04-19 21:36:55 +0430

    Update alu_controller.v

--- alu_controller.v
+++ alu_controller.v
@@ -9,6 +9,7 @@ module alu_controller(alu_op , func , alu_operation);
     parameter MTYPE = 2'b00; // Memorey accses
     parameter BTYPE = 2'b01; // Branch
     parameter RTYPE = 2'b10; // Register
+    parameter JTYPE = 2'b11; //Jump
 
     parameter ADD = 6'b100000;
     parameter SUB = 6'b100010;
@@ -48,9 +49,14 @@ module alu_controller(alu_op , func , alu_operation);
                         alu_operation = `SLT;
                     end
 
-                    default : break; // it is for jr instruct because jr opcode is 0000000 and func is 001000
+                    default : alu_operation = `OFF; 
                 endcase
             end
+
+            JTYPE: begin
+                alu_operation = `OFF;
+            end
+
         endcase
     end
 endmodule
