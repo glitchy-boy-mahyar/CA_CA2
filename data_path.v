72ac9de2a8a2843adc29da9070648d1d8ea239ce
Alireza Salamat <52575958+alirezasalamat@users.noreply.github.com>
2020-04-19 12:11:18 +0430

    mux outputs fixed

--- data_path.v
+++ data_path.v
@@ -14,28 +14,28 @@ module data_path(reg_dst , jal_reg , pc_to_reg , alu_src , mem_to_reg ,
     wire [31:0] pc_out , instructs , read_data , read_data_1 , read_data_2 , alu_result , sign_ext_out ,
                 adder_1_out , adder_2_out , shifter_1_out , shifter_2_out;
     //output of multiplexers
-    wire [4:0] mux1 , mux2;
-    wire [31:0] mux3 , mux4 , mux5 , mux6 , mux7 , mux8;
+    wire [4:0] mux_reg_dst_out , mux_jal_reg_out;
+    wire [31:0] mux_pc_to_reg_out , mux_alu_src_out , mux_mem_to_reg_out , mux_jump_sel_out , mux_pc_jump_out , mux_pc_src_out;
 
 
-    pc program_counter(mux7 , pc_out , clk , rst);
+    pc program_counter(mux_pc_jump_out , pc_out , clk , rst);
     instruction_mem inst_mem(pc_out , instructs);
-    register_file reg_file(instructs[25:21] , instructs[20:16] , mux2 , mux3, reg_write , read_data_1 , read_data_2 , clk);
-    alu alu_unit(read_data_1 , mux4 , alu_result , ZERO , alu_cntrl);
+    register_file reg_file(instructs[25:21] , instructs[20:16] , mux_jal_reg_out , mux_pc_to_reg_out, reg_write , read_data_1 , read_data_2 , clk);
+    alu alu_unit(read_data_1 , mux_alu_src_out , alu_result , ZERO , alu_cntrl);
     data_mem data_memory(alu_result , read_data_2 , read_data , mem_read , mem_write , clk);
     sign_ext_16_to_32 sign_extension(instructs[15:0] , sign_ext_out);
     adder adder_1(`FOUR , pc_out , adder_1_out);
     adder adder_2(adder_1_out , shifter_1_out , adder_2_out);
     shift_left_2 sfl1(sign_ext_out , shifter_1_out);
     shifter_for_jump sfl2(instructs , pc_out , shifter_2_out);
-    mux_5bit mux_reg_dst(instructs[20:16] , instructs[15:11] , mux1 , reg_dst);
-    mux_5bit mux_jal_reg(mux1 , `THIRTY_ONE , mux2 , jal_reg);
-    mux_32bit mux_pc_to_reg(mux5 , adder_1_out , mux3 , pc_to_reg);
-    mux_32bit mux_alu_src(read_data_2 , sign_ext_out , mux4 , alu_src);
-    mux_32bit mux_mem_to_reg(alu_result , read_data , mux5 , mem_to_reg);
-    mux_32bit mux_jump_sel(read_data_1 , shifter_2_out, mux6 , jump_sel);
-    mux_32bit mux_pc_jump(mux8 , mux6 , mux7 , pc_jump);
-    mux_32bit mux_pc_src(adder_1_out , adder_2_out , mux8 , pc_src);
+    mux_5bit mux_reg_dst(instructs[20:16] , instructs[15:11] , mux_reg_dst_out , reg_dst);
+    mux_5bit mux_jal_reg(mux_reg_dst_out , `THIRTY_ONE , mux_jal_reg_out , jal_reg);
+    mux_32bit mux_pc_to_reg(mux_mem_to_reg_out , adder_1_out , mux_pc_to_reg_out , pc_to_reg);
+    mux_32bit mux_alu_src(read_data_2 , sign_ext_out , mux_alu_src_out , alu_src);
+    mux_32bit mux_mem_to_reg(alu_result , read_data , mux_mem_to_reg_out , mem_to_reg);
+    mux_32bit mux_jump_sel(read_data_1 , shifter_2_out, mux_jump_sel_out , jump_sel);
+    mux_32bit mux_pc_jump(mux_pc_src_out , mux_jump_sel_out , mux_pc_jump_out , pc_jump);
+    mux_32bit mux_pc_src(adder_1_out , adder_2_out , mux_pc_src_out , pc_src);
 
 
 
